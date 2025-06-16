e224fe9586ba07a46e749c6617dec32ad303cb49
devikaagarwal <devikaagarwal83@gmail.com>
2025-06-16 08:52:14 +0530

    changes

--- CORE-JAVA/Control-Flow-Level/LEVEL-3/BMICalculator.java
+++ CORE-JAVA/Control-Flow-Level/LEVEL-3/BMICalculator.java
@@ -14,14 +14,14 @@ public class BMICalculator {
 
         System.out.printf("Your BMI is: %.2f\n", bmi);
 
-        if (bmi < 18.5)
+        if (bmi <= 18.4)
             System.out.println("Underweight");
-        else if (bmi < 24.9)
-            System.out.println("Normal weight");
-        else if (bmi < 29.9)
+        else if (bmi >=18.5 && bmi <=24.9)
+            System.out.println("Normal");
+        else if (bmi >= 25.0 && bmi <= 39.9)
             System.out.println("Overweight");
         else
-            System.out.println("Obesity");
+            System.out.println("Obese");
 
         input.close();
     }
