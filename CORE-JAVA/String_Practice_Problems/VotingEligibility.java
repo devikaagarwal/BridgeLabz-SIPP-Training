4deede2cb5a8c6109059b310cf2ad870116f9d45
devikaagarwal <devikaagarwal83@gmail.com>
2025-06-19 11:04:41 +0530

    Generate random student ages and check voting eligibility

---
+++ CORE-JAVA/String_Practice_Problems/VotingEligibility.java
@@ -0,0 +1,36 @@
+import java.util.Random;
+
+public class VotingEligibility {
+
+    public static int[] generateAges(int n) {
+        Random rand = new Random();
+        int[] ages = new int[n];
+        for (int i = 0; i < n; i++) {
+            ages[i] = rand.nextInt(90);
+        }
+        return ages;
+    }
+
+    public static String[][] checkVoting(int[] ages) {
+        String[][] result = new String[ages.length][2];
+        for (int i = 0; i < ages.length; i++) {
+            result[i][0] = String.valueOf(ages[i]);
+            result[i][1] = (ages[i] >= 18) ? "Can Vote" : "Cannot Vote";
+        }
+        return result;
+    }
+
+    public static void displayTable(String[][] data) {
+        System.out.println("Age\tEligibility");
+        for (String[] row : data) {
+            System.out.println(row[0] + "\t" + row[1]);
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] ages = generateAges(10);
+        String[][] result = checkVoting(ages);
+        displayTable(result);
+    }
+}
+
