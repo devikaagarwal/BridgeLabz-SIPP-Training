43ee3b2752f64f887224871e151d60cffde6885e
devikaagarwal <devikaagarwal83@gmail.com>
2025-06-26 09:56:36 +0530

    usage of keywords such as this,static,final and instanceOf.

---
+++ OOPS/JAVA_KEYWORDS/Employee_Management.java
@@ -0,0 +1,29 @@
+package OOPS.JAVA_KEYWORDS;
+
+class Employee {
+    static String companyName = "Tech Solutions Ltd.";
+    static int totalEmployees = 0;
+
+    String name, designation;
+    final int id;
+
+    Employee(String name, int id, String designation) {
+        this.name = name;
+        this.id = id;
+        this.designation = designation;
+        totalEmployees++;
+    }
+
+    static void displayTotalEmployees() {
+        System.out.println("Total Employees: " + totalEmployees);
+    }
+
+    void displayDetails() {
+        if (this instanceof Employee) {
+            System.out.println("Name: " + name);
+            System.out.println("ID: " + id);
+            System.out.println("Designation: " + designation);
+            System.out.println("Company: " + companyName);
+        }
+    }
+}
