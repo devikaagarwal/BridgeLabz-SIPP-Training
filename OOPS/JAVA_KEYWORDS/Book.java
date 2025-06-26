6baf3fd635508e6982dcf6b7a03fb52fb9a6f570
devikaagarwal <devikaagarwal83@gmail.com>
2025-06-26 09:49:17 +0530

    define usage of keywords such as this,final,static and instanceOf.

---
+++ OOPS/JAVA_KEYWORDS/Book.java
@@ -0,0 +1,27 @@
+package OOPS.JAVA_KEYWORDS;
+
+class Book {
+    static String libraryName = "Central Library";
+
+    final String isbn;
+    String title, author;
+
+    Book(String title, String author, String isbn) {
+        this.title = title;
+        this.author = author;
+        this.isbn = isbn;
+    }
+
+    static void displayLibraryName() {
+        System.out.println("Library: " + libraryName);
+    }
+
+    void displayDetails() {
+        if (this instanceof Book) {
+            System.out.println("Title: " + title);
+            System.out.println("Author: " + author);
+            System.out.println("ISBN: " + isbn);
+        }
+    }
+}
+
