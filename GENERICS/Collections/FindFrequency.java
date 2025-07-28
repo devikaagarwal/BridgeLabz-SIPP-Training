f7441681350e9a65e1d2573374c8be61a4c47778
devikaagarwal <devikaagarwal83@gmail.com>
2025-07-28 11:50:55 +0530

    add method to count frequency of elements in a list and return as Map.

---
+++ GENERICS/Collections/FindFrequency.java
@@ -0,0 +1,18 @@
+package GENERICS.Collections;
+
+import java.util.*;
+public class FindFrequency {
+    public static Map<String, Integer> findFrequency(List<String> list) {
+        Map<String, Integer> freqMap = new HashMap<>();
+        for (String item : list) {
+            freqMap.put(item, freqMap.getOrDefault(item, 0) + 1);
+        }
+        return freqMap;
+    }
+
+    public static void main(String[] args) {
+        List<String> words = Arrays.asList("apple", "banana", "apple", "orange");
+        Map<String, Integer> freqMap = findFrequency(words);
+        System.out.println("Frequency Map: " + freqMap);
+    }
+}
